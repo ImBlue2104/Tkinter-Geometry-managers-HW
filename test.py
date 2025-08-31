ff09f7aca48e7661761becc7e1d02a64c7d76328
Neel Patil <neel.rp21@gmail.com>
2025-08-31 14:52:12 -0700

    fixed

--- test.py
+++ test.py
@@ -7,8 +7,12 @@ root.geometry("300x200")
 my_frame = Frame(root, bg="lightblue", width=200, height=100)
 my_frame.pack(pady=20)
 
+# Prevent the frame from shrinking to fit its contents
+my_frame.pack_propagate(False)
+
 # Add a button inside the frame
 btn = Button(my_frame, text="Click Me")
 btn.pack()
 
 root.mainloop()
+
